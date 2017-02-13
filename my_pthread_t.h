d6f32a5040c5ebf2b69382a7f6bfb93460fab898
rshnn-mutalisk <roshanpatel06@gmail.com>
2017-02-13 12:40:26 -0500

    made changes to some data structs.  added readme

--- my_pthread_t.h
+++ my_pthread_t.h
@@ -1,9 +1,9 @@
-/*
+/************************************************************************************************************
 *	cs518 - assignment01 
 *	thread scheduling 
 *
 *	my_pthread_t.h 	
-*/
+************************************************************************************************************/
 
 
 #ifndef MY_PTHREAD_T_H
@@ -20,29 +20,16 @@
 
 
 #define PAGE_SIZE 4096;
-#define PRIORITY_NUM
+#define PRIORITY_LEVELS 5
 #define TIME_QUANTUM 50000                      // 50 ms 
 #define MAINTENENCE_TIME 10*TIME_QUANTUM
 
 
-
-
-/*
-	THREAD_STATES 
-*/
-typedef enum __state__ {
-
-	EMBRYO,         // In process of being initialized  
-	READY,          // Ready to be scheduled 
-	RUNNING,        // Currently executing 
-// 	BLOCKED,        // A thread that is blocked waiting for a lock 
-	WAITING,        // A thread that is waiting indefinietly for another thread to perform an action (yield?)
-	TERMINATED,     // Exited.  Terminated 
-	ZOMBIE          // Defined as zombie process 
-
-}state;
- 
-
+/************************************************************************************************************
+*
+*    PTHREAD DATA STRUCTURES
+*
+************************************************************************************************************/
 
 /*
 	MY_PTHREAD_T
@@ -58,121 +45,129 @@ typedef struct my_pthread_t {
 
 
 typedef struct my_pthread_attr_t {
-	// empty? lol
 }my_pthread_attr_t;
 
 
 /*
 	MY_PTHREAD_MUTEX_T
-		lock 				binary lock (0: unlocked; 1: locked)
-		owning_thread 		pointer to the mutex's owning thread 	 
 */
 typedef struct my_pthread_mutex_t { 
     
     int                         initialized;    // for mutex_init() 
     unsigned int                id;
 	int 					    lock;
-	struct my_pthread_t* 	    owning_thread;
-	struct scheduler_unit_t*    waiting_queue;
+	struct my_pthread_t* 	    owning_thread;	// Do we instead need a pointer to thread_unit?  Or both?
+	struct thread_unit_list_*   waiting_queue;
 
 }my_pthread_mutex_t;
 
 
 
 typedef struct my_pthread_mutexattr_t {
-	// lol also empty 
 }my_pthread_mutexattr_t;
 
 
 
-/**
-* 
+/************************************************************************************************************
 *
-*    SCHEDULER DATA STRUCTS
+*    SCHEDULER DATA STRUCTURES
 *
-* 
+************************************************************************************************************/
+
+/*
+	STATES 
 */
+typedef enum state_ {
 
-typedef struct thread_god{
-    
-    struct scheduler_unit_t * 	thread_unit;        //array of size NUM_PRIORITIES
-    ucontext_t          		context;
-    
-}thread_god;
+	EMBRYO,         // In process of being initialized  
+	READY,          // Ready to be scheduled 
+	RUNNING,        // Currently executing 
+// 	BLOCKED,        // A thread that is blocked waiting for a lock 
+	WAITING,        // A thread that is waiting indefinitely for another thread to perform an action (yield?)
+	TERMINATED,     // Exited.  Terminated 
+	ZOMBIE          // Defined as zombie process 
+
+}state;
 
 
-
-typedef struct scheduler_unit_t{
+ 
+/*
+	THREAD_UNIT
+		Contains meta-data about each pthread for scheduling 
+		Doubles as a node structure for thread_unit_list 
+*/
+typedef struct thread_unit_ {
     
     my_pthread_t*       		thread;
     ucontext_t          		context;
     state               		state;          // NOTE: do we need to keep info on why its waiting here?
 	int                 		time_slice;
 	int                 		run_count;
-    struct scheduler_unit_t*    waiting_on_me;  // rename later
+    struct thread_unit_list_*   waiting_on_me;  // Linked list of thread_units currently waiting on this thread
     
-}scheduler_unit_t;
+    struct thread_unit_* 		next;
+}thread_unit;
 
 
 
-typedef struct mutex_god{
-    
-    my_pthread_mutex_t*     head;
-    
-}mutex_god;
+/*
+	THREAD_UNIT_LIST
+		A linked list of thread_units 
+*/
+typedef struct thread_unit_list_ {
 
+	struct thread_unit_* 		head;
+	struct thread_unit_* 		tail;
+	int 						size;
+
+}thread_unit_list_;
+
+
+
+
+/* 
+	MUTEX_NODE
+		Contains a my_pthread_mutex & pointer to next
+		Used by thread_scheduler to store a linked list of mutexes
+*/
+typedef struct mutex_node_ {
+
+	my_pthread_mutex_t* 	mutex;
+	my_pthread_mutex_t* 	next;
+
+}mutex_node;
 
 
 
 
 /*
-    COMMENTS FROM GCHAT
-    
-    
-    move things out of my_pthread_t (into scheduler?)
-        protect data structure from user 
-    5 levels
-    
-    DATASTRUCTURE FOR SCHEDULER 
-
-    NOTE:  have some sort of cheeck at the end of pthread_create to make sure all the data structs were initialized properly 
-
-        
-    
-    Saad: I will mainly write cause mic being weird but
-    If we have a doubly linked list as our structure. Would we run one list of threads? And then go to the next list of threads? (one list is equivalent to a specific priority)
-    But any new thread is highest priority, so by default, it should be added to the first linked list, no? (in our case, we would have 5 linked lists cause 5 priority levels)
-    Here is one way: after each thread runs (1 thread is like 1 index/node in the linked list), we would check the whole structure. So, if we have covered all of priority 0, then we go
-    into priority 1. Now, if running one thread then a new thread comes in. New thread comes in priority 0. That means after the current thread (which is in priority 1 is done) we would 
-    check if anything in priority 0 and then run that
-
-    From what I understand: maintenance cycle is to update the extremely low priority threads to be upggraded to a higher priority
-    I think I understand
-    
-    What we could do is keep track of how many time units it has run. This way we can also scale priorities up based on time units (when running maintenance)
-    It would be easier if we give our own definition of fair: fairness = time units given? fairness = selection based on level?
-    
-    Which is why it should be based is amount run. E.g. if in lowest priority list there are 2 threads. One (T1) has run 50 units and another (T2) has run 200 units. 
-    During maintenance cycle, T1 should be upgraded all the way to possibly priority 1 or 2. While T2 should be upgraded to priority 3 or so.
-    
-    Same here - would prefer table cause easy indexing
-
-    Yeah, there should be a standard equation or something like that (so we can also justify that they are all adjusted based on some rule)
-
-        new_time_slice = LVL_0_TIME_SLICE * priority_level * 1.5
-        
-        
-    Array of linked list
-
+	SCHEDULER
+		Main data structure of the scheduler thread.
+		Contains priority array
+			Array[PRIORITY_LEVELS]
+			Each index contains a linked list of thread_units
+		Contains mutex_list
+			A linked list of mutex_nodes
+		Stores ucontexts of main thread and scheduler thread
 */
+typedef struct scheduler_ {
+    
+    struct thread_unit_list_ 	priority_array[PRIORITY_LEVELS];
+    struct mutex_node_* 		mutex_list;
+    ucontext_t          		scheduler_ucontext;
+    ucontext_t					main_ucontext;
+
+}scheduler;
 
 
 
 
+/************************************************************************************************************
+*
+*    FUNCTION LIBRARY 
+*
+************************************************************************************************************/
 
-
-
-/**********************************************************************/
 
 /* my_pthread core library */
 int my_pthread_create( my_pthread_t * thread, my_pthread_attr_t * attr, void *(*function)(void*), void * arg);
--- readme.md
+++ readme.md
@@ -4,9 +4,9 @@ cs-518 osdesign
 
 
 
-#### dev-notes
+## dev-notes
 
->rrp78 Fri 10 Feb 2017 03:11:50 PM EST
+### rshnn Fri 10 Feb 2017 03:11:50 PM EST
 
 Boiler plated the header and .c file.  Still need to flesh out the structs we're using.  
 
@@ -28,5 +28,99 @@ So far, only made three structs:
 + `struct my_pthread_mutex_attr_t`
     * ????????
 
-
 I'll try to find some time later to flesh out a basic scheduler struct
+
+
+### rshnn Mon 13 Feb 2017 09:16:47 AM EST
+
+Meeting Notes | Sunday 2016.02.12
+
++ Saad had issues with his microphone.  Look into other outlets for future calls 
++ Pair programmed with kodia (or something)
+    * View into single file 
++ Data structure discussion
+    * Priority levels will be represented as as array 
+        - Each index of the array will represent a priority. (Array[0] = highest)
+        - Each index of the array will contain a linked list of thread_units (linked list structure)
+            + Thread_units will contain a bunch of info about a thread
+                * its ucontext, its state, pointer to pthread_t, time slice, run_count, etc. 
+    * The scheduler will manage things using this structure 
++ NOTE:  Have some sort of check before you run pthread_create() to make sure the scheduler is initialized correctly 
++ Consider separating the scheduler from my_pthread library 
+
+**data structures**
+
+* pthread library 
+    - `my_pthread_t`
+        + dh624: We should keep the user away from important info about this structure.  Do not put ucontext or state in here. 
+        + Agreed.  All that logistic stuff was moved into data structures gears toward the scheduler 
+        + Contains: 
+            * `unsigned int     threadID`
+            * `void *           return_val`
+            * `int              priority`
+
+
+    - `my_pthread_mutex`
+        + contains:
+            * `int initialized`
+            * `unsigned int id`
+            * `int lock`
+                - state of lock
+            * `my_pthread_t* owning_thread`
+                - Will we instead need a pointer to the thread_unit?
+                - Or both?
+            * `thread_unit_list* waiting_queue`
+
+* scheduler data structures 
+
+    - `state`
+        + enumeration 
+            * EMBRYO
+            * READY
+            * RUNNING
+            * WAITING
+                - Includes when a thread is yielding/joined and when blocked by a lock
+            * TERMINATED
+            * ZOMBIE
+        + States gathered from an old project dh624 had
+                    
+    - `thread_unit` _(was named scheduler_unit_t)_
+        + Contains: 
+            * `my_pthread_t*    thread`
+            * `ucontext_t       context`
+            * `state            state`
+            * `int              time_slice`
+            * `int              run_count`
+            * `struct           thread_unit* waiting_on_me`
+                - linked list of thread_units waiting on this thread to finish
+            * `thread_unit*     next`
+                - Makes it a node structure for thread_unit_list
+    
+    - `thread_unit_list`
+        + list of thread units
+        + Contains:
+            * `thread_unit* head`
+            * `thread_unit* tail`
+            * `int size`
+
+    - `mutex_node`
+        + Why not just turn my_ptread_mutex_t into a node?
+            * Trying to keep those structures away from users
+            * Same reason as why we have a separate thread_unit struct to keep my_pthread_t free of data we dont want the user to see 
+        + Contains:  
+            * `my_pthread_mutex_t*  mutex`
+            * `my_pthread_mutex_t   next`
+
+    - `scheduler`  _(was named thread_god)_
+        + Main struct that keeps track of the priority array and mutexes
+        + Contains: 
+            * `thread_unit_list priority_array[PRIORITY_LEVELS]`
+                - An array, length PRIORITY_LEVELS, whose indexes each contain a list of thread_units
+                - Each index will represent its own priority level
+                - Each thread_unit in the list located at that particular index will be considered at that priority level
+                - Ex.  All thread_units in `priority_array[2].thread_unit_list ` will be of priority level 2.
+            * `mutex_node* mutex_list`
+                + List of mutexes
+                + Do we need separate lists for locked and unlocked mutexes?
+            - `ucontext_t scheduler_ucontext`
+            - `ucontext_t main_ucontext`
