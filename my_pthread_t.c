dfeb80231611e16e650bf4b433d585dfc968049a
rshnn-mutalisk <roshanpatel06@gmail.com>
2017-02-25 18:51:22 -0500

    debugging

--- my_pthread_t.c
+++ my_pthread_t.c
@@ -93,6 +93,9 @@ void scheduler_runThread(thread_unit* thread, thread_unit* prev){
 		return;
 	}
 
+
+	_print_thread_unit(thread);
+	_print_thread_unit(prev);
 	// if(scheduler->currently_running->state == RUNNING){
 	// 	scheduler->currently_running->state = READY;
 	// }
