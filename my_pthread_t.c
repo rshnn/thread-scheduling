c72a77f340ae4ca5d13691230b4f417af047d019
rshnn-mutalisk <roshanpatel06@gmail.com>
2017-02-25 18:37:09 -0500

    debugging

--- my_pthread_t.c
+++ my_pthread_t.c
@@ -235,7 +235,7 @@ void maintenance_cycle(){
 				
 				// free(temp->ucontext->uc_stack.ss_sp);
 				printf("success 1\n");
-				free(temp->ucontext);
+				// free(temp->ucontext);
 				printf("success 2\n");
 				free(temp);  // Free the thread_unit in maint_cycle after ucontext 
 				printf("success 3\n");
